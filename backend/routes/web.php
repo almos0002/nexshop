27350ed6dfbcaec55933d18963ccfbe3c216ee83
Almos Mali <almos0002@gmail.com>
2025-02-25 15:34:07 +0545

    routes auth

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,15 +16,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // product dashboard route
+    Route::get('/product', [ProductController::class, 'list'])->name('product.list');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/{uuid}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/{uuid}/update', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{uuid}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
 });
-
-
-// product dashboard route
-Route::get('/product', [ProductController::class, 'list'])->name('product.list');
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{uuid}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{uuid}/update', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{uuid}/delete', [ProductController::class, 'destroy'])->name('product.destroy');
 
 require __DIR__ . '/auth.php';
