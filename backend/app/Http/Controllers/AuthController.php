16b3f5036e2e769a4ee482dd9f3789b040c28558
Almos Mali <almos0002@gmail.com>
2025-02-27 16:04:17 +0545

    auth api

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            // 'access_token' => $token,
+            // 'token_type' => 'Bearer',
+            'id' => $user->id,
+            'name' => $user->name,
+            'status' => 'Login Successfull',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logout successful']);
+    }
+
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            // 'access_token' => $token,
+            // 'token_type' => 'Bearer',
+            'id' => $user->id,
+            'name' => $user->name,
+            'status' => 'Register successful',
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,8 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store']);
-Route::post('/register', [App\Http\Controllers\Auth\RegisteredUserController::class, 'store']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
 Route::apiResource('products', ProductController::class)->only(['index', 'show']);
 Route::apiResource('orders', OrderController::class)->only(['index', 'store', 'show']);
